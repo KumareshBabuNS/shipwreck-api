718d8d4fa3e912b22fd4dacca5ada0d786e0ffa9
Dadonas <dadonas@gmail.com>
2017-10-21 18:24:44 -0200

    Angular UI added

--- src/main/java/com/dadonas/crud/controller/ProductController.java
+++ src/main/java/com/dadonas/crud/controller/ProductController.java
@@ -1,5 +1,14 @@
 package com.dadonas.crud.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ProductController {
+	
+	@RequestMapping("/")
+	public String load() {
+		return "Hello Spring Controller!!!";
+	}
 
 }
